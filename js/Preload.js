457bcffb276afbe3a445e6d3f88c4cba4b1f9c0c
iva <iva@localhost.localdomain>
2014-12-18 16:45:59 +0000

    map refinement

--- js/Preload.js
+++ js/Preload.js
@@ -6,13 +6,13 @@ TopDownGame.Preload = function(){};
 TopDownGame.Preload.prototype = {
   preload: function() {
     //show loading screen
-    this.preloadBar = this.add.sprite(this.game.world.borderLeft, this.game.world.centerY, 'preloadbar');
+    this.preloadBar = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'preloadbar');
     this.preloadBar.anchor.setTo(0.5);
 
     this.load.setPreloadSprite(this.preloadBar);
 
     //load game assets
-    this.load.tilemap('level1', 'assets/tilemaps/level1bigger.json', null, Phaser.Tilemap.TILED_JSON);
+    this.load.tilemap('level1', 'assets/tilemaps/level1.json', null, Phaser.Tilemap.TILED_JSON);
     this.load.image('gameTiles', 'assets/images/tiles.png');
     this.load.image('greencup', 'assets/images/greencup.png');
     this.load.image('bluecup', 'assets/images/bluecup.png');
--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 var TopDownGame = TopDownGame || {};
 
-TopDownGame.game = new Phaser.Game(250, 250, Phaser.AUTO, '');
+TopDownGame.game = new Phaser.Game(200, 200, Phaser.AUTO, '');
+TopDownGame.map = new Phaser.Game(360, 360, Phaser.AUTO, '');
 
 TopDownGame.game.state.add('Boot', TopDownGame.Boot);
 TopDownGame.game.state.add('Preload', TopDownGame.Preload);
