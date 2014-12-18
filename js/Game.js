a16cd91b70593fb8e1155adf387a514354bd5b5a
Andi24 <andrei_masca93@yahoo.com>
2014-12-18 17:47:41 +0200

    Added constructor for the friend type; some functionality still incomplete

--- js/Game.js
+++ js/Game.js
@@ -120,6 +120,41 @@ Policy = function( minLength, minNums, minPunct, minSpeChar, colour){
   // there was no need for methods, as the attributes are set to public
 }
 
+/*constructor for the Friend object
+  There can be more than one object of this type
+  We should be able to instantiate him at a given set of coordinates*/
+Friend = function(currentX, currentY){
+
+  //location
+  this.currentX = currentX;
+  this.currentY = currentY;
+
+  //speed attribute set to a default of 10.0
+  this.speed = 10.0;
+  // password paper set to null by default; will be assigned a clone of the actual object upon player's approval
+  this.passwordNote = null;
+  // permission to follow player; can be set to true after player interacts with the friend
+  this.permission = false;
+
+  this.followPlayer = function(Player){
+    //upon the player moving, we need to check in what direction he's gone to and make the friend move after him 
+    // need to be careful so that he actually moves after the player, and doesn't just teleport near him.
+  }
+
+  // function to be called when player agrees to cooperate
+  this.getApproval = function(note){
+    // friend gets the object (needs changed to clone the object, not a direct reference to it)
+    this.passwordNote = note;
+    // permission is set to true, so that he can now follow the player around
+    this.permission = true;
+  }
+
+  //method that alters friend's speed when the player's speed is altered; speed is the speed of the player
+  this.changeSPeed = function(speed){
+    this.speed = speed;
+  }
+}
+
 TopDownGame.Game.prototype = {
   create: function() {
     this.map = this.game.add.tilemap('level1');
