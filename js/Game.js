3ccf9d8e432312431bc2c5bf520b4b2b7af3b2a0
Andi24 <andrei_masca93@yahoo.com>
2014-12-18 17:20:15 +0200

    Added a constructor for the Policy object

--- js/Game.js
+++ js/Game.js
@@ -98,6 +98,28 @@ Player = function(){
   }
 }
 
+/* constructor for the policy object 
+    Upon creation of objects on the map, individual policies will be created, then each door will be assigned one policy
+    Each time the user tries to set a new password for a door, the door checks if the password subscribes to the given policy
+    */
+Policy = function( minLength, minNums, minPunct, minSpeChar, colour){
+  // the attributes of the policy object should be the specifications for how passwords should look like
+  //minimum length of a password
+  this.minLength = minLength;
+  // maximum length of a password set to a default of 15; should be changed
+  this.maxLength = 15;
+  // minimum number of numbers
+  this.minNums = minNums;
+  // minimum number of punctuation signs
+  this.minPunct = minPunct;
+  // minimum number of special characters, i.e. @, #, %, ^, &, *, ~
+  this.minSpeChar = minSpeChar
+  // colour corresponding to the policy
+  this.colour = colour;
+
+  // there was no need for methods, as the attributes are set to public
+}
+
 TopDownGame.Game.prototype = {
   create: function() {
     this.map = this.game.add.tilemap('level1');
