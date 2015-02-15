07a73e3244751701f0ae287c92475ff643f2136a
ivababukova <ibabukova@gmail.com>
2015-02-15 22:18:43 +0000

    smaller enemy sprite

--- js/Preload.js
+++ js/Preload.js
@@ -28,44 +28,20 @@ Encrypt.Preload.prototype = {
     this.load.image ('AntiKeyLog', 'assets/images/GameIcons/AntiKeyLogger.png');
     this.load.image ('info', 'assets/images/GameIcons/Info.png');
     this.load.image ('antivirus', 'assets/images/GameIcons/AntiVirus.png');
-
-    this.load.image ('enemy', 'assets/images/Virus.png');
-
-    // player and door animations
-    this.load.spritesheet ('player', 'assets/images/32x32ExampleSprite.png', 64, 64, 30);
-    this.load.spritesheet ('frontDoor','assets/images/Animations/DoorAnimations/FrontDoorAnimationFrames.png', 64, 64, 17, 0, 0 );
-    this.load.spritesheet ('sideDoor', 'assets/images/Animations/DoorAnimations/SideDoorAnimationFrames.png', 128, 128, 17, 0, 0);
-
-    this.load.tilemap('level1', 'assets/tilemaps/map32x32.json', null, Phaser.Tilemap.TILED_JSON);
-
-    // images on the map:
-    this.load.image ('32x32TileSet1Encrypt', 'assets/EncryptTileSets/32x32TileSet1Encrypt.png');
-    this.load.image ('clue', 'assets/images/GameIcons/Clue.png');
-    this.load.image ('door', 'assets/images/32pixelPlayer.png');
-    this.load.image ('ceiling', 'assets/images/32pixelPlayer.png');
-    this.load.image ('bluePolicy', 'assets/images/GameIcons/PasswordPolicySheetBlue.png');
-    this.load.image ('redPolicy', 'assets/images/GameIcons/PasswordPolicySheetRed.png');
-    this.load.image ('greenPolicy', 'assets/images/GameIcons/PasswordPolicySheetGreen.png');
-    this.load.image ('magentaPolicy', 'assets/images/GameIcons/PasswordPolicySheetMagenta.png');
-    this.load.image ('yellowPolicy', 'assets/images/GameIcons/PasswordPolicySheetYellow.png');
-    this.load.image ('firewall', 'assets/images/GameIcons/Firewall.png');
-    this.load.image ('AntiKeyLog', 'assets/images/GameIcons/AntiKeyLogger.png');
-    this.load.image ('info', 'assets/images/GameIcons/Info.png');
-    this.load.image ('antivirus', 'assets/images/GameIcons/AntiVirus.png');
-    this.load.image ('enemy', 'assets/images/Virus.png');
+    this.load.image ('winkey', 'assets/images/winkey.png');
     this.load.image ('frontBlock', 'assets/images/GameIcons/frontDoorBlock.png');
     this.load.image ('sideBlock', 'assets/images/GameIcons/sidewaysDoorBlock.png');
-    this.load.image ('winkey', 'assets/images/winkey.png');
 
-    // animations for the player and the doors:
+    // player, enemy and door animations
     this.load.spritesheet ('player', 'assets/images/32x32ExampleSprite.png', 64, 64, 30);
     this.load.spritesheet ('frontDoor','assets/images/Animations/DoorAnimations/FrontDoorAnimationFrames.png', 64, 64, 17, 0, 0 );
     this.load.spritesheet ('sideDoor', 'assets/images/Animations/DoorAnimations/SideDoorAnimationFrames.png', 128, 128, 17, 0, 0);
+    this.load.image ('enemy', 'assets/images/32pixelPlayer.png');
+
 
     //main menu images
     this.load.image ('space', 'assets/images/BackDrop.png');
     this.load.image ('space2', 'assets/images/MLetters.png');
-    this.load.image ('gameover', 'assets/images/GameOver.png');
 
     // buttons:
     this.load.image('backButton', 'assets/images/gobackButtonIva.png');
