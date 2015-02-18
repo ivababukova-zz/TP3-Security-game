60942c1093464bbfe3faf8204f5e9d6d0bb2f13d
ivababukova <ibabukova@gmail.com>
2015-02-18 19:43:13 +0000

    attempt on highlighting the buttons on the main menu

--- js/MainMenu.js
+++ js/MainMenu.js
@@ -9,20 +9,18 @@ Encrypt.MainMenu.prototype = {
 
     create: function() {
 	
-		//play main menu music
-	    this.music = this.game.add.audio('music');
-        this.music.play();
+		    //play main menu music
+	      this.music = this.game.add.audio ('music');
+        this.music.play ();
 
         //show the space tile, repeated: - (BMDK - Updated to our backdrop)
-        this.background = this.game.add.tileSprite(0, 0, this.game.width, this.game.height, 'space');
-        //
-        this.foreground = this.game.add.tileSprite(0, 0, this.game.width, this.game.height, 'space2');
-
+        this.background = this.game.add.tileSprite (0, 0, this.game.width, this.game.height, 'space');
+        this.foreground = this.game.add.tileSprite (0, 0, this.game.width, this.game.height, 'space2');
 
         //give it speed in x
-        this.background.autoScroll(-20, 0);
+        this.background.autoScroll (-20, 0);
         //BMDK:- Letters are given other direction of scrolling
-        this.foreground.autoScroll(0, 20);
+        this.foreground.autoScroll (0, 20);
 
         text = "WELCOME TO 3NCRYPT, PLAYA' \n HAI BRYAN, HEN xoxo";
         style = {font: "35px Serif", fill: "#fff", align: "center"};
@@ -33,23 +31,40 @@ Encrypt.MainMenu.prototype = {
         text =  "You have just discovered that you are on a spaceship\n" +
                 "The cruel aliens kidnapped you here :(\n" +
                 "Don't worry, there is hope, you can ESCAPE\n" +
-                "See how on the instructions page\n" +
-                "And dun farget tae bring yer knife eh";
+                "See how on the instructions page.\n\n" +
+                "click twice one of the buttons below:\n";
         style = {font: "20px Serif", fill: "#fff", align: "center"};
-        var bestLabel = this.game.add.text(this.game.width / 2, this.game.height - 350, text, style);
-        bestLabel.anchor.set(0.5);
+        welcomeLabel = this.game.add.text(this.game.width / 2, this.game.height - 350, text, style);
+        welcomeLabel.anchor.set(0.5);
 
 
-        var instructionsButton = this.game.add.button(this.game.width/2 -70, this.game.height - 240, 'instructionsButton', this.showInstructions, this);
-        var startButton = this.game.add.button(this.game.width/2 - 73, this.game.height - 150, 'startButton', this.startGame, this);
+        this.pressedInstrButton = this.game.add.button (this.game.width/2 -70, this.game.height - 240, 'pressedInstrButton', this.unpress, this);
+        // this.pressedInstrButton.inputEnabled = false;
+        this.instrButton = this.game.add.button (this.game.width/2 -70, this.game.height - 240, 'instructionsButton', this.showInstructions, this);
+
+        this.pressedStartButton = this.game.add.button (this.game.width/2 - 73, this.game.height - 150, 'pressedStartButton', this.startGame, this);
+        this.startButton = this.game.add.button (this.game.width/2 - 73, this.game.height - 150, 'startButton', this.hidethisButton, this);
     },
 
-    showInstructions: function(){
-        this.game.state.start('Instructions');
+    showInstructions: function () {
+        this.instrButton.renderable = false;
+        this.instrButton.inputEnabled = false;
+        //this.time.events.add(10000, this.instrButton.renderable = true);
+        //this.game.state.start ('Instructions');
+        //this.instrButton.renderable = true;
     },
 
-    startGame: function(){
-		this.music.stop();
-        this.game.state.start('Game');
+    unpress: function () {
+        this.game.state.start ('Instructions');
+    },
+
+    hidethisButton: function () {
+        this.startButton.renderable = false;
+        this.startButton.inputEnabled = false;
+    },
+
+    startGame: function () {
+		    this.music.stop ();
+        this.game.state.start ('Game');
     }
 };
--- js/Preload.js
+++ js/Preload.js
@@ -12,18 +12,17 @@ Encrypt.Preload.prototype = {
     this.load.setPreloadSprite(this.preloadBar);
 
     //load game assets
-
     this.load.tilemap ('level1', 'assets/tilemaps/map32x32.json', null, Phaser.Tilemap.TILED_JSON);
-
     this.load.image ('32x32TileSet1Encrypt', 'assets/EncryptTileSets/32x32TileSet1Encrypt.png');
 
-    // pickable items:
-    this.load.image ('clue', 'assets/images/GameIcons/Clue.png');
+    // policies:
     this.load.image ('bluePolicy', 'assets/images/GameIcons/PasswordPolicySheetBlue.png');
     this.load.image ('redPolicy', 'assets/images/GameIcons/PasswordPolicySheetRed.png');
     this.load.image ('greenPolicy', 'assets/images/GameIcons/PasswordPolicySheetGreen.png');
     this.load.image ('magentaPolicy', 'assets/images/GameIcons/PasswordPolicySheetMagenta.png');
     this.load.image ('yellowPolicy', 'assets/images/GameIcons/PasswordPolicySheetYellow.png');
+    // others:
+    this.load.image ('clue', 'assets/images/GameIcons/Clue.png');
     this.load.image ('firewall', 'assets/images/GameIcons/Firewall.png');
     this.load.image ('AntiKeyLog', 'assets/images/GameIcons/AntiKeyLogger.png');
     this.load.image ('info', 'assets/images/GameIcons/Info.png');
@@ -38,26 +37,27 @@ Encrypt.Preload.prototype = {
     this.load.spritesheet ('sideDoor', 'assets/images/Animations/DoorAnimations/SideDoorAnimationFrames.png', 128, 128, 17, 0, 0);
     this.load.spritesheet ('enemy', 'assets/images/Animations/EnemySpriteSheet.png', 64, 64, 5); // BMDK: - Added enemy spritesheet
 
-
     //main menu images
     this.load.image ('space', 'assets/images/BackDrop.png');
     this.load.image ('space2', 'assets/images/MLetters.png');
 
     // buttons:
+    // this.load.spritesheet ('instructionsButt', 'assets/images/ButtonImages/instrButtonSprite.png', 64, 130);
     this.load.image ('backButton','assets/images/ButtonImages/gobackButtonIva.png');
     this.load.image ('instructionsButton','assets/images/ButtonImages/instrButton.png');
-    this.load.image ('pressedInstructionsButton','assets/images/ButtonImages/pressedInstrButton.png');
+    this.load.image ('pressedInstrButton','assets/images/ButtonImages/pressedInstrButton.png');
     this.load.image ('restartButton','assets/images/ButtonImages/restart_button.png');
     this.load.image ('startButton','assets/images/ButtonImages/startButton.png');
+    this.load.image ('pressedStartButton','assets/images/ButtonImages/pressedStartButton.png');
     this.load.image ('hintsButton','assets/images/ButtonImages/hintsButton.png');
     this.load.image ('pressedHintsButton','assets/images/ButtonImages/pressedHintsButton.png');
 
 
     //music
-    this.load.audio('music', 'assets/sounds/main_music.ogg');
+    this.load.audio ('music', 'assets/sounds/main_music.ogg');
     //this.load.audio('passwordSound', 'assets/sounds/door_sound.wav');
-    this.load.audio('pickUpSound', 'assets/sounds/pickup_sound.ogg');
-	this.load.audio('doorSound', 'assets/sounds/door_open.ogg');
+    this.load.audio ('pickUpSound', 'assets/sounds/pickup_sound.ogg');
+	  this.load.audio ('doorSound', 'assets/sounds/door_open.ogg');
 
   },
   create: function() {
