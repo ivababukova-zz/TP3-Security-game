c4157fc466adecbc135fb730496ee727e73668cc
Colonel Dumpling <bmdkenny@gmail.com>
2015-02-22 02:00:02 +0000

    added php function to Game.js
    function for writing password to DB added but needs refined.

--- js/Game.js
+++ js/Game.js
@@ -1086,12 +1086,21 @@ getEntropy: function (pwdFeed) {
 
 //Blank Section below by BMDK for DB function setup
 
-
-
-
-
-
-
+ function storePasswordToDB(pwd, entropy) {
+    if (pwd === "") {
+        return;
+    } else {
+        if (window.XMLHttpRequest) {
+            // code for IE7+, Firefox, Chrome, Opera, Safari
+            xmlhttp = new XMLHttpRequest();
+        } else {
+            // code for IE6, IE5
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        xmlhttp.open("GET","storepassword.php?p="+pwd+"&ent="+entropy,true);
+        xmlhttp.send();
+    }
+}
 
 
 
