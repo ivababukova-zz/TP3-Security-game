00acb759a4d087bd6bc5b9c98a2df7031a4c9178
Colonel Dumpling <bmdkenny@gmail.com>
2015-03-04 17:32:02 +0000

    missed one

--- storebadpassword.php
+++ storebadpassword.php
@@ -5,8 +5,8 @@ include_once("connect.php");
 $did = intval($_GET['did']);
 
 //insert passwords that don't match those set on the doors in the game
-$sql="INSERT INTO `teamr1415`.`UsersBadPwdEntries` (`pid`, `uid`, `did`, `sid`) VALUES ('"
-	.$_SESSION["currentpid"]."', '".$_SESSION["uid"]."', '".$did."', '".$_SESSION["sid"]."');";
+$sql="INSERT INTO `teamr1415`.`UsersBadPwdEntries` (`pid`, `uid`, `did`, `sid`, `gaid`) VALUES ('"
+	.$_SESSION["currentpid"]."', '".$_SESSION["uid"]."', '".$did."', '".$_SESSION["sid"]."', '".$_SESSION["gaid"]."');";
 mysqli_query($conn,$sql);
 mysqli_close($conn);
 ?>
